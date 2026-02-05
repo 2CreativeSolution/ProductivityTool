4ca4a2f12627eccb81892b1a05e33a772425c47a
nxame <git@nxa.me>
2026-02-05 16:20:14 +0100

    fix: minor ui adjustment welcome header

--- web/src/components/WelcomeSection/WelcomeSection.jsx
+++ web/src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,8 +1,4 @@
-// src/components/WelcomeSection/WelcomeSection.js
-
 import React, { useState, useEffect } from 'react'
-
-import { useQuery, gql } from '@redwoodjs/web'
 
 import { useAuth } from 'src/auth'
 
@@ -37,7 +33,7 @@ const WelcomeSection = () => {
   const Name = currentUser?.name || currentUser?.email || 'User'
 
   return (
-    <div className="relative z-30 mb-8 mt-32 flex flex-col items-start justify-between md:flex-row md:items-center">
+    <div className="relative z-30 mb-8 mt-8 flex flex-col items-start justify-between md:flex-row md:items-center">
       <div>
         <h1 className="text-2xl font-bold text-gray-900">Welcome, {Name}!</h1>
         <p className="mt-1 text-gray-600">{currentTime}</p>
